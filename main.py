09a7210bf81dcc239e753b147f56cfabe550736c
Matt Auerbach <tennisfan0526@gmail.com>
2013-11-25 14:39:36 -0500

    database and perms

--- main.py
+++ main.py
@@ -12,10 +12,25 @@ def index():
     if session.get('venmo_token'):
         return 'Your Venmo token is %s' % session.get('venmo_token')
     else:
-        return redirect('https://api.venmo.com/oauth/authorize?client_id=%s&scope=make_payments,access_profile&response_type=code' % CONSUMER_ID)
+        return redirect('https://api.venmo.com/oauth/authorize?client_id=%s&scope=make_payments,access_profile,access_email,access_phone&response_type=code' % CONSUMER_ID)
 
 @app.route('/oauth-authorized')
 def oauth_authorized():
+    #connect to DB 
+    import os
+    import psycopg2
+    import urlparse
+
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
     AUTHORIZATION_CODE = request.args.get('code')
     data = {
         "client_id":CONSUMER_ID,
@@ -25,11 +40,18 @@ def oauth_authorized():
     url = "https://api.venmo.com/oauth/access_token"
     response = requests.post(url, data)
     response_dict = response.json()
+
+
     access_token = response_dict.get('access_token')
     user = response_dict.get('user')
 
+
     session['venmo_token'] = access_token
     session['venmo_username'] = user['username']
+
+    name = user['name']
+    email = user['email']
+    phone= user['phone'] 
 
     return 'You were signed in as %s' % user['username']
 
